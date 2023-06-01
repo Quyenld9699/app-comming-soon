f1d9b7d8f9523752e5ad62e82ca0efd87633b07b
Le Dinh Quyen <quyenld9699@gmail.com>
2023-06-01 14:30:44 +0700

    maintain ui

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
             >
                 <img src={imagePath.LOGO_FULL_HORIZONTAL} alt="" style={{ maxWidth: '500px', width: '100%' }} />
                 <div style={{ textTransform: 'uppercase', fontWeight: '500', letterSpacing: '2px', fontSize: '30px', marginTop: '30px' }}>
-                    we're coming soon
+                    We will be back soon!
                 </div>
                 <div className="divisor"></div>
-                <div style={{ marginBottom: '30px' }}>We're working hard to give you the best experience!</div>
+                <div style={{ marginBottom: '30px' }}>We're upgrading to give you the best experience!</div>
                 <SocialPart />
             </div>
         </div>
